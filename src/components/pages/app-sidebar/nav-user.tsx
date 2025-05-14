cc0bb867bd218b7f1e58e3b344b2d2040f9e1a68
sampaiogabriel <gabrielsampaiolima@hotmail.com>
2025-05-14 10:25:57 -0300

    chore: add avatar and dropdown menu with clerk informations

--- src/components/pages/app-sidebar/nav-user.tsx
+++ src/components/pages/app-sidebar/nav-user.tsx
@@ -1,20 +1,121 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { SidebarMenu, SidebarMenuItem } from "@/components/ui/sidebar";
-import { LogIn } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useClerk, useUser } from "@clerk/nextjs";
+import { BadgeCheck, ChevronsUpDown, LogIn, LogOut } from "lucide-react";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 export const NavUser = () => {
+  const { user, isLoaded } = useUser();
+  const { isMobile, open } = useSidebar();
+
+  const { openUserProfile, signOut } = useClerk();
+
   return (
     <SidebarMenu>
       <SidebarMenuItem>
-        <div className="p-2">
-          <Link href="/auth/sign-in" passHref className="w-full">
-            <Button size="sm" variant="outline" className="w-full">
-              <LogIn />
-              Entrar
-            </Button>
-          </Link>
-        </div>
+        {user ? (
+          <div
+            className={cn(
+              "flex items-center gap-2",
+              !open && "flex-col-reverse"
+            )}
+          >
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton
+                  size="lg"
+                  className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                >
+                  <Avatar>
+                    <AvatarImage src={user.imageUrl} />
+                    <AvatarFallback>{user.fullName} </AvatarFallback>
+                  </Avatar>
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-semibold">
+                      {user.fullName}
+                    </span>
+                    <span className="truncate text-xs">
+                      {user.primaryEmailAddress?.emailAddress}
+                    </span>
+                  </div>
+                  <ChevronsUpDown className="ml-auto size-4" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                className="w-[var(--radix-dropdown-menu-trigger-width)] min-w-56 rounded-lg"
+                side={isMobile ? "bottom" : "right"}
+                align="end"
+                sideOffset={4}
+              >
+                <DropdownMenuLabel className="p-0 font-normal">
+                  <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                    <Avatar>
+                      <AvatarImage src={user.imageUrl} />
+                      <AvatarFallback>{user.fullName} </AvatarFallback>
+                    </Avatar>
+
+                    <div className="grid flex-1 text-left text-sm leading-tight">
+                      <span className="truncate font-semibold">
+                        {user.fullName}
+                      </span>
+                      <span className="truncate text-xs">
+                        {user.primaryEmailAddress?.emailAddress}
+                      </span>
+                    </div>
+                  </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem onClick={() => openUserProfile()}>
+                    <BadgeCheck />
+                    Gerenciar conta
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem onClick={() => signOut()}>
+                    <LogOut />
+                    Sair
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        ) : (
+          <>
+            {!isLoaded ? (
+              <Skeleton />
+            ) : (
+              <div className="p-2">
+                <Link href="/auth/sign-in" passHref className="w-full">
+                  <Button size="sm" variant="outline" className="w-full">
+                    <LogIn />
+                    Entrar
+                  </Button>
+                </Link>
+              </div>
+            )}
+          </>
+        )}
       </SidebarMenuItem>
     </SidebarMenu>
   );
