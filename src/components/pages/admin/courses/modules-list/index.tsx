cde9b0b0f387083d2ac0b5272492224b0df9be2d
sampaiogabriel <gabrielsampaiolima@hotmail.com>
2025-05-20 17:23:38 -0300

    feat: add drag n drop for lists (lessons and class)

--- src/components/pages/admin/courses/modules-list/index.tsx
+++ src/components/pages/admin/courses/modules-list/index.tsx
@@ -8,6 +8,12 @@ import { Tooltip } from "@/components/ui/tooltip";
 import { LessonsList } from "./lessons-list";
 import { LessonFormItem, ManageLessonDialog } from "./manage-lesson-dialog";
 import { AlertDialog } from "@/components/ui/alert-dialog";
+import {
+  DragDropContext,
+  Draggable,
+  DropResult,
+  Droppable,
+} from "@hello-pangea/dnd";
 
 export const ModulesList = () => {
   const {
@@ -21,11 +27,17 @@ export const ModulesList = () => {
   const [lessonToEdit, setLessonToEdit] = useState<LessonFormItem | null>(null);
   const [moduleIndex, setModuleIndex] = useState(0);
 
-  const { fields, remove } = useFieldArray({
+  const { fields, remove, move } = useFieldArray({
     control,
     name: "modules",
     keyName: "_id",
   });
+
+  const handleDragEnd = ({ source, destination }: DropResult) => {
+    if (destination) {
+      move(source.index, destination.index);
+    }
+  };
 
   return (
     <div className="col-span-full flex flex-col">
@@ -48,80 +60,103 @@ export const ModulesList = () => {
       )}
 
       {!!fields.length && (
-        <div className="overflow-hidden flex flex-col gap-4 mt-6">
-          {fields.map((field, index) => (
-            <div
-              key={field.id}
-              className="w-full grid grid-cols-[40px_1fr] items-center bg-muted/50 rounded-md overflow-hidden border border-input"
-            >
-              <div className="w-full h-full bg-muted/50 flex items-center justify-center">
-                <GripVertical />
-              </div>
-              <div className="w-full h-full flex flex-col gap-6 p-4">
-                <div className="flex items-center justify-between">
-                  <div>
-                    <p className="text-xl font-bold">{field.title}</p>
-                    <p className="text-sm text-muted-foreground">
-                      {field.description}
-                    </p>
-                  </div>
-
-                  <div className="flex items-center gap-3">
-                    <Tooltip content="Editar módulo">
-                      <Button
-                        variant="outline"
-                        size="icon"
-                        onClick={() => {
-                          setModuleToEdit(field);
-                          setShowManageModuleDialog(true);
-                        }}
+        <DragDropContext onDragEnd={handleDragEnd}>
+          <Droppable droppableId="modules">
+            {(provided: any) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className="overflow-hidden flex flex-col gap-4 mt-6"
+              >
+                {fields.map((field, index) => (
+                  <Draggable
+                    key={`module-item-${field._id}`}
+                    draggableId={`module-item-${field._id}`}
+                    index={index}
+                  >
+                    {(provided: any) => (
+                      <div
+                        {...provided.draggableProps}
+                        ref={provided.innerRef}
+                        className="w-full grid grid-cols-[40px_1fr] items-center bg-muted/50 rounded-md overflow-hidden border border-input"
                       >
-                        <Pen />
-                      </Button>
-                    </Tooltip>
+                        <div
+                          {...provided.dragHandleProps}
+                          className="w-full h-full bg-muted/50 flex items-center justify-center"
+                        >
+                          <GripVertical />
+                        </div>
+                        <div className="w-full h-full flex flex-col gap-6 p-4">
+                          <div className="flex items-center justify-between">
+                            <div>
+                              <p className="text-xl font-bold">{field.title}</p>
+                              <p className="text-sm text-muted-foreground">
+                                {field.description}
+                              </p>
+                            </div>
 
-                    <Tooltip content="Excluir módulo">
-                      <AlertDialog
-                        title="Excluir módulo"
-                        description="Tem certeza que deseja excluir este módulo? Isso irá excluir todas as aulas deste módulo."
-                        onConfirm={() => remove(index)}
-                      >
-                        <Button variant="outline" size="icon">
-                          <Trash />
-                        </Button>
-                      </AlertDialog>
-                    </Tooltip>
+                            <div className="flex items-center gap-3">
+                              <Tooltip content="Editar módulo">
+                                <Button
+                                  variant="outline"
+                                  size="icon"
+                                  onClick={() => {
+                                    setModuleToEdit(field);
+                                    setShowManageModuleDialog(true);
+                                  }}
+                                >
+                                  <Pen />
+                                </Button>
+                              </Tooltip>
 
-                    <Button
-                      variant="outline"
-                      onClick={() => {
-                        setModuleIndex(index);
-                        setShowManageLessonDialog(true);
-                      }}
-                    >
-                      Adicionar Aula <Plus />
-                    </Button>
-                  </div>
-                </div>
+                              <Tooltip content="Excluir módulo">
+                                <AlertDialog
+                                  title="Excluir módulo"
+                                  description="Tem certeza que deseja excluir este módulo? Isso irá excluir todas as aulas deste módulo."
+                                  onConfirm={() => remove(index)}
+                                >
+                                  <Button variant="outline" size="icon">
+                                    <Trash />
+                                  </Button>
+                                </AlertDialog>
+                              </Tooltip>
 
-                <LessonsList
-                  moduleIndex={index}
-                  onEditLesson={(lesson) => {
-                    setModuleIndex(index);
-                    setLessonToEdit(lesson);
-                    setShowManageLessonDialog(true);
-                  }}
-                />
+                              <Button
+                                variant="outline"
+                                onClick={() => {
+                                  setModuleIndex(index);
+                                  setShowManageLessonDialog(true);
+                                }}
+                              >
+                                Adicionar Aula <Plus />
+                              </Button>
+                            </div>
+                          </div>
 
-                {!!errors?.modules?.[index]?.lessons?.message && (
-                  <p className="text-sm text-destructive">
-                    {errors?.modules?.[index]?.lessons?.message}
-                  </p>
-                )}
+                          <LessonsList
+                            moduleIndex={index}
+                            onEditLesson={(lesson) => {
+                              setModuleIndex(index);
+                              setLessonToEdit(lesson);
+                              setShowManageLessonDialog(true);
+                            }}
+                          />
+
+                          {!!errors?.modules?.[index]?.lessons?.message && (
+                            <p className="text-sm text-destructive">
+                              {errors?.modules?.[index]?.lessons?.message}
+                            </p>
+                          )}
+                        </div>
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
               </div>
-            </div>
-          ))}
-        </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       )}
 
       {!!errors?.modules?.message && (
--- src/components/pages/admin/courses/modules-list/lessons-list.tsx
+++ src/components/pages/admin/courses/modules-list/lessons-list.tsx
@@ -5,6 +5,12 @@ import { GripVertical, Pen, Trash } from "lucide-react";
 import { useFieldArray, useFormContext } from "react-hook-form";
 import { LessonFormItem } from "./manage-lesson-dialog";
 import { AlertDialog } from "@/components/ui/alert-dialog";
+import {
+  DragDropContext,
+  Draggable,
+  DropResult,
+  Droppable,
+} from "@hello-pangea/dnd";
 
 type LessonsListProps = {
   moduleIndex: number;
@@ -17,11 +23,17 @@ export const LessonsList = ({
 }: LessonsListProps) => {
   const { control } = useFormContext<CreateCourseFormData>();
 
-  const { fields, remove } = useFieldArray({
+  const { fields, remove, move } = useFieldArray({
     control,
     name: `modules.${moduleIndex}.lessons`,
     keyName: "_id",
   });
+
+  const handleDragEnd = ({ source, destination }: DropResult) => {
+    if (destination) {
+      move(source.index, destination.index);
+    }
+  };
 
   return (
     <div className="p-4 rounded-md bg-muted">
@@ -31,45 +43,70 @@ export const LessonsList = ({
         </p>
       )}
 
-      <div className="overflow-hidden flex flex-col gap-2">
-        {fields.map((field, index) => (
-          <div
-            key={field.id}
-            className="w-full grid grid-cols-[30px_1fr] items-center bg-card/50 rounded-md overflow-hidden border border-input"
-          >
-            <div className="w-full h-full bg-muted/50 flex items-center justify-center">
-              <GripVertical size={14} />
-            </div>
-
-            <div className="w-full h-full flex items-center justify-between gap-4 p-3">
-              <p className="font-semibold">{field.title}</p>
-
-              <div className="flex items-center gap-3">
-                <Tooltip content="Editar aula">
-                  <Button
-                    variant="outline"
-                    size="icon"
-                    onClick={() => onEditLesson(field)}
+      {!!fields.length && (
+        <DragDropContext onDragEnd={handleDragEnd}>
+          <Droppable droppableId="lessons">
+            {(provided) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className="overflow-hidden flex flex-col gap-2"
+              >
+                {fields.map((field, index) => (
+                  <Draggable
+                    key={`module-lessons-item-${field._id}`}
+                    draggableId={`module-lessons-item-${field._id}`}
+                    index={index}
                   >
-                    <Pen />
-                  </Button>
-                </Tooltip>
-                <Tooltip content="Excluir aula">
-                  <AlertDialog
-                    title="Excluir aulas"
-                    description="Tem certeza que deseja excluir esta aula?"
-                    onConfirm={() => remove(index)}
-                  >
-                    <Button variant="outline" size="icon">
-                      <Trash />
-                    </Button>
-                  </AlertDialog>
-                </Tooltip>
+                    {(provided) => (
+                      <div
+                        {...provided.draggableProps}
+                        ref={provided.innerRef}
+                        className="w-full grid grid-cols-[30px_1fr] items-center bg-card/50 rounded-md overflow-hidden border border-input"
+                      >
+                        <div
+                          {...provided.dragHandleProps}
+                          className="w-full h-full bg-muted/50 flex items-center justify-center"
+                        >
+                          <GripVertical size={14} />
+                        </div>
+
+                        <div className="w-full h-full flex items-center justify-between gap-4 p-3">
+                          <p className="font-semibold">{field.title}</p>
+
+                          <div className="flex items-center gap-3">
+                            <Tooltip content="Editar aula">
+                              <Button
+                                variant="outline"
+                                size="icon"
+                                onClick={() => onEditLesson(field)}
+                              >
+                                <Pen />
+                              </Button>
+                            </Tooltip>
+                            <Tooltip content="Excluir aula">
+                              <AlertDialog
+                                title="Excluir aulas"
+                                description="Tem certeza que deseja excluir esta aula?"
+                                onConfirm={() => remove(index)}
+                              >
+                                <Button variant="outline" size="icon">
+                                  <Trash />
+                                </Button>
+                              </AlertDialog>
+                            </Tooltip>
+                          </div>
+                        </div>
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
               </div>
-            </div>
-          </div>
-        ))}
-      </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      )}
     </div>
   );
 };
